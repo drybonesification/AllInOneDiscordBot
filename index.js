1c47f118b665e94d0245a09bd3590d0755ce933d
Chuy <jesus.saavedra37926637@gmail.com>
2020-05-28 12:26:07 -0700

    aye you can kick now

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { token, prefix } = require("./config.json");
+const { bot_token, prefix, giphy_token } = require("./config.json");
+
+var GphApiClient = require("giphy-js-sdk-core");
+client = GphApiClient(giphy_token);
 
 client.on("ready", () => {
   console.log("Connected as " + client.user.tag);
@@ -12,4 +15,19 @@ client.on("ready", () => {
   });
 });
 
-client.login(token);
+client.on("message", (message) => {
+  if (message.member.hasPermission(["KICK_MEMBERS", "BAN_MEMBERS"])) {
+    //   console.log(message.content);
+    if (message.content.startsWith(`${prefix}kick`)) {
+      // message.channel.send("Test Kick");
+      let member = message.mentions.members.first();
+      member.kick().then((member) => {
+        message.channel.send(
+          ":wave:" + member.displayName + " has been kicked"
+        );
+      });
+    }
+  }
+});
+
+client.login(bot_token);
